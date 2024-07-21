c927cd7b05d1b78ffb9f8858bfd06ea330974daf
Vedant <borodevedant4@gmail.com>
2024-07-21 15:36:19 +0530

    react basic

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,10 @@
+
+
+
+function App() {
+  return (
+    <h1>Hello World | Vedant here</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,10 @@
+
+
+function App() {
+
+  return (
+    <h1>Chai aur react with vite | Vedant</h1>
+  )
+}
+
+export default App
