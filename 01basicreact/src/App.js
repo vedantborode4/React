ca41e161865a00b68129dae7492447b127aae29f
Vedant <borodevedant4@gmail.com>
2024-07-21 21:21:15 +0530

    understood react flow and basic stucture

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,9 +1,12 @@
 
-
+import Chai from "./Chai";
 
 function App() {
   return (
+  <>
+    <Chai/>
     <h1>Hello World | Vedant here</h1>
+  </>
   );
 }
 
---
+++ 01basicreact/src/Chai.jsx
@@ -0,0 +1,7 @@
+
+function Chai (){
+
+   return(<h3>Chai is Best</h3>) 
+}
+
+export default Chai
