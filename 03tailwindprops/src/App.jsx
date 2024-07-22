e652b985b6618cbbb462f97564e9795091c22b4b
Vedant <borodevedant4@gmail.com>
2024-07-22 19:02:26 +0530

    tailwind + probs

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  let myObj = {
+    username:"Vedant",
+    age: 20,
+  }
+  return (
+    <>
+      <h1 className="bg-green-400 text-black p-4 rounded-xl mb-4">Tailwind Test</h1>
+      <Card username="Elon" btnText = "contact"/>
+      <Card username="Sam"/>
+    </>
+  )
+}
+
+export default App
