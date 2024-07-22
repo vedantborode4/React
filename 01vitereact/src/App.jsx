01d27a2eb01298bd7d81eabd65e7bdd4eb167c87
Vedant <borodevedant4@gmail.com>
2024-07-22 12:13:18 +0530

    Custom React added

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,11 +1,12 @@
 import Chai from "./Chai"
 
 function App() {
-
+  
+  const username = "Vedant"
   return (<>
   
   <Chai/>
-    <h1>Chai aur react with vite | Vedant</h1>
+    <h1>Chai aur react with vite | {username}</h1>
   </>
   )
 }
