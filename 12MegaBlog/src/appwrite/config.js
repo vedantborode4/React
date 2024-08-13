0a41d800e044e31296416999799eaf3e0ce07cd5
Vedant <borodevedant4@gmail.com>
2024-08-13 23:50:39 +0530

    building services with help of class

---
+++ 12MegaBlog/src/appwrite/config.js
@@ -0,0 +1,23 @@
+import conf from "../conf/conf";
+import {Client, ID, Databases, Storage, Query} from "appwrite"
+
+export class Service{
+    client = new Client();
+    databases;
+    bucket;
+
+    constructor(){
+        this.client
+        .setEndpoint(conf.appwriteUrl)
+        .setProject(conf.appwritePojectId)
+        this.databases = new Databases(this.client)
+        this.bucket = new Storage(this.client)
+    }
+    async createPost(){
+
+    }
+}
+
+const service = new Service()
+
+export default service;
